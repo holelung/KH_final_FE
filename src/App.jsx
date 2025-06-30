1859c559d02c36ca16337390ef12110d017890fc
HiDId <152678114+TheHiDId@users.noreply.github.com>
2025-06-30 17:09:12 +0900

    Ready Before Start Work

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import "./App.css";
 import { AuthProvider } from "./Context/AuthContext";
 import { Routes, Route } from "react-router-dom";
+import Main from "./pages/Home/main/Main";
 import Test from "./pages/Home/Test";
+import Header from "./pages/include/header/Header";
+import Footer from "./pages/include/footer/Footer";
+import Navbar from "./pages/include/navbar/Navbar";
 
 function App() {
   return (
     <AuthProvider>
       <Routes>
-        <Route path="/" element={<Test />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </AuthProvider>
   );
