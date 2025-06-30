d7c2b944bebb20e042d85446008582f6a82b658c
HiDId <152678114+TheHiDId@users.noreply.github.com>
2025-06-30 17:50:36 +0900

    ??

--- src/pages/Home/main/Main.jsx
+++ src/pages/Home/main/Main.jsx
@@ -1,7 +1,14 @@
 const Main = () => {
   return (
     <>
-      <div></div>
+      <div className="w-screen h-dvh">
+        <div className="absolute top-0 w-full h-12 z-5 bg-gray-400"></div>
+        <div className="absolute top-0 left-0 w-48 h-full z-10 bg-white"></div>
+        <div className="absolute bottom-0 w-full h-32 z-5 bg-gray-400"></div>
+        <div className="absolute top-12 left-48 w-[calc(100%-192px)] min-h-[calc(100%-48px)] bg-gray-200">
+          <div className="w-[calc(100%-60px)] h-full m-8 bg-white flex justify-center items-center">요소</div>
+        </div>
+      </div>
     </>
   );
 };
