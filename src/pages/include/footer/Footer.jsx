905df3fbfe8a956d5a70bc3873d2d7a45bd50bb9
HiDId <152678114+TheHiDId@users.noreply.github.com>
2025-07-01 16:38:08 +0900

    Include Component Process

--- src/pages/include/footer/Footer.jsx
+++ src/pages/include/footer/Footer.jsx
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
     <>
-      <div></div>
+      <div className="absolute bottom-0 w-full h-32 z-5 bg-saintragray font-PretendardM flex justify-center items-center">
+        <div className="w-4xl h-full flex justify-start items-center gap-4">뭔가 들어갈 장소</div>
+      </div>
     </>
   );
 };
--- src/pages/include/header/Header.jsx
+++ src/pages/include/header/Header.jsx
@@ -1,7 +1,13 @@
 const Header = () => {
   return (
     <>
-      <div></div>
+      <div className="absolute top-0 w-full h-12 z-5 bg-saintralightblue shadow-md font-PretendardM flex justify-center items-center box-border">
+        <div className="w-4xl h-full flex justify-end items-center gap-4">
+          <div className="size-8 border rounded-full flex justify-center items-center">1</div>
+          <div className="size-8 border rounded-full flex justify-center items-center">2</div>
+          <div className="size-8 border rounded-full flex justify-center items-center">3</div>
+        </div>
+      </div>
     </>
   );
 };
