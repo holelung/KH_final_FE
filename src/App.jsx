2df25ced126aa93e63363d32de757fded756235e
HiDId <152678114+TheHiDId@users.noreply.github.com>
2025-07-16 17:10:28 +0900

    Authlayout Change

--- src/App.jsx
+++ src/App.jsx
@@ -25,18 +25,22 @@ import ApproveJoin from "./pages/admin/ApproveJoin";
 import Log from "./pages/admin/Log";
 import UserManage from "./pages/admin/UserManage";
 import Grid from "./Components/layout/grid/Grid";
-import AuthContainer from "./Components/authlayout/authcontainer/AuthContainer";
-import Login from "./Components/authlayout/login/Login";
-import Registration from "./Components/authlayout/registration/Registration";
+import Authenticator from "./pages/auth/authenticator/Authenticator";
+import Login from "./Components/auth/Login";
+import PasswordReset from "./Components/auth/PasswordReset";
+import Registration from "./Components/auth/Registration";
+import ReissuePassword from "./Components/auth/ReissuePassword";
 
 function App() {
   return (
     <AuthProvider>
       <ToastContainer position="bottom-right" />
       <Routes>
-        <Route path="/authenticator" element={<AuthContainer />}>
+        <Route path="/authenticator" element={<Authenticator />}>
           <Route index element={<Login />} />
           <Route path="registration" element={<Registration />} />
+          <Route path="reissue-password" element={<ReissuePassword />} />
+          <Route path="password-reset" element={<PasswordReset />} />
         </Route>
 
         <Route
